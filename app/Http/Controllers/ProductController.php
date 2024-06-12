a02ca098a64af36c98324c5381c1fd8e6587dc2e
Khinesuwai_01 <khinesuwai9812@gmail.com>
2024-06-13 01:01:24 +0630

    Product(CRU)

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,4 +14,32 @@ class ProductController extends Controller
         // dd($data);
         return view('product.product',compact('data'));
     }
+    public function create(){
+        return view(('product.create'));
+    }
+    public function store(Request $request){
+        // $data = $request->all();
+        // dd($data);
+        Product::create([
+            'name'=> $request->name,
+            'price'=> $request->price,
+            'color'=> $request->color,
+        ]);
+        return Redirect::route('productProduct');
+    }
+    public function edit($id){
+        $data = Product::where('id', $id)->first();
+        return view('product.edit', compact('data'));
+    }
+    public function update(Request $request, $id){
+        $data = Product::where('id', $id)->first();
+        $data->name =   $request->name;
+        $data->price = $request->price;
+        $data->color = $request->color;
+    
+        $data->update();
+    
+        return redirect()->route('productProduct');
+    
+    }
 }
